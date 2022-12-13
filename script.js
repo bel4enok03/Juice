84739a8a0afd6f3e45ac6e82a933f1a8f74d7189
bel4enok03 <belskaya-tanyush@mail.ru>
2022-12-14 01:38:17 +0400

    *

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+gsap.from('.homeText',{opacity:0,duration:2,delay:0.9})
+gsap.from('.contentHeading',{duration:1.5,delay:0.6, x: '-100vw'})
+gsap.from('.homeImage',{duration:1.5,delay:0.6, x: '+100vw'})
+gsap.from('.btn, .btnProduct', { opacity: 0, scale: 0, rotation: 360,duration:1.9 }, { duration: 2, delay: 4.3, opacity: 1, scale: 1, rotation: 0 })
+
+gsap.from('.menu',{opacity:0,duration:5,delay:1,stagger:0.6})
+gsap.from('.contentText',{duration:6,delay:0.6, x: '+100vw', opacity:0})
+gsap.from('.aboutImage',{duration:6,delay:.6, x: '-100vw', opacity:0})
+gsap.from('.contactInfo',{duration:7,delay:.6, y: '+100vw', opacity:0})
+gsap.from('.heading',{ duration: 2, delay: 1.5, x: '-100vw', ease: 'power2.in', opacity: 0 })
 
 let menu = document.querySelector("#menu-btn");
 let navbar = document.querySelector(".header .navbar");
@@ -11,4 +21,5 @@ window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
+
 
